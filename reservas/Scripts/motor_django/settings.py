c6d53259060392e19a9694293214db4e2ec5095e
Benja0g <benjamj3012@gmail.com>
2026-09-04 01:30:05 -0400

    creación de la pagina de error 404

--- reservas/Scripts/motor_django/settings.py
+++ reservas/Scripts/motor_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0evtza#%lc8w@mw7l57g4m7y(!7sityuf^uwa2c-v%-5^htvb4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
